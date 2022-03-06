c6602546aa575e8c584587aacbf89680136d81dc
harwcam <84109744+harwcam@users.noreply.github.com>
2022-03-06 12:25:03 -0500

    Task submit clears after initial entry

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,41 @@
-var buttonEl = document.querySelector("#save-task");
+var formEl = document.querySelector("#task-form");
 var tasksToDoEl = document.querySelector("#tasks-to-do");
 
-var createTaskHandler = function() {
+var taskFormHandler = function() {
+
+    event.preventDefault();
+    var taskNameInput = document.querySelector("input[name='task-name']").value;
+    var taskTypeInput = document.querySelector("select[name='task-type']").value;
+
+    if (!taskNameInput || !taskTypeInput) {
+        alert("You need to fill out the task form!");
+        return false;
+    }
+    
+    formEl.reset();
+
+    var taskDataObj = {
+        name : taskNameInput,
+        type: taskTypeInput
+    };
+
+    createTaskEl(taskDataObj);
+    formEl.reset()
+
+}
+
+var createTaskEl = function(taskDataObj){
+
     var listItemEl = document.createElement("li");
     listItemEl.className = "task-item";
-    listItemEl.textContent = "This is a new task.";
+
+    var taskInfoEL = document.createElement("div");
+    taskInfoEL.className ="task-info";
+    taskInfoEL.innerHTML = "<h3 class='task-name'>" + taskDataObj.name + "</h3><span class='task-type'>" + taskDataObj.type + "</span>";
+
+    listItemEl.appendChild(taskInfoEL)
     tasksToDoEl.appendChild(listItemEl);
+    
 }
-buttonEl.addEventListener("click", createTaskHandler);
+
+formEl.addEventListener("submit", taskFormHandler);
